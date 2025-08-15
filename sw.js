baf28622dd1005e38efda90129ab1aba34ca2e91
iyaanvip <jbmpiistorereal@gmail.com>
2025-08-15 20:28:46 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+// sw.js
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  const url = event.notification.data || '/';
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
+      for (let client of windowClients) {
+        // if a window with the URL is already open, focus it
+        if (client.url === url && 'focus' in client) return client.focus();
+      }
+      if (clients.openWindow) return clients.openWindow(url);
+    })
+  );
+});
