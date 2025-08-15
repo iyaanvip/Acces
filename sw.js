ba95c80b1fcdaa57940908702e2d03e561ba99ca
iyaanvip <jbmpiistorereal@gmail.com>
2025-08-15 21:27:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,13 +2,12 @@
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
 
-self.addEventListener('notificationclick', function(event) {
+self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   const url = event.notification.data || '/';
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
-      for (let client of windowClients) {
-        // if a window with the URL is already open, focus it
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(list => {
+      for (const client of list) {
         if (client.url === url && 'focus' in client) return client.focus();
       }
       if (clients.openWindow) return clients.openWindow(url);
